5e787eda43e3f413affe04a4e529a48324a51a46
Cillian <baugercillianpro@gmail.com>
2026-03-13 14:12:02 +0100

    fix pokemon classé

--- src/main/java/org/example/pokemon/logic/DamageCalculator.java
+++ src/main/java/org/example/pokemon/logic/DamageCalculator.java
@@ -1,4 +1,5 @@
 package org.example.pokemon.logic;
 
 public class DamageCalculator {
+
 }
--- src/main/java/org/example/pokemon/model/Pokemon.java
+++ src/main/java/org/example/pokemon/model/Pokemon.java
@@ -1,34 +1,28 @@
 package org.example.pokemon.model;
 
-import java.util.ArrayList;
-
 public class Pokemon {
     private int id;
     private String name;
     private int hp;
-    private int attackStat;
-    private ArrayList<SpecialAttack> attacks;
+    private int attack;
     private int defense;
-    private int attackSpeStat;
-    private ArrayList<SpecialAttack> attackSpes;
+    private int specialAttack;
     private int specialDefense;
     private int speed;
-    private Type type1;
-    private Type type2;
+    private String type1;
+    private String type2;
     private String imageUrl;
 
-    public Pokemon(int id, String name, int hp, ArrayList<SpecialAttack> attacks, int defense,
-                   int attackStat, int attackSpeStat,ArrayList<SpecialAttack> attackSpes , int specialDefense,
-                   int speed, Type type1, Type type2, String imageUrl) {
+    public Pokemon(int id, String name, int hp, int attack, int defense,
+                   int specialAttack, int specialDefense,
+                   int speed, String type1, String type2, String imageUrl) {
 
         this.id = id;
         this.name = name;
         this.hp = hp;
-        this.attacks = attacks;
-        this.attackStat = attackStat;
-        this.attackSpeStat =attackSpeStat;
+        this.attack = attack;
         this.defense = defense;
-        this.attackSpes = attackSpes;
+        this.specialAttack = specialAttack;
         this.specialDefense = specialDefense;
         this.speed = speed;
         this.type1 = type1;
@@ -40,16 +34,15 @@ public class Pokemon {
     public int getId() { return id; }
     public String getName() { return name; }
     public int getHp() { return hp; }
-    public ArrayList<SpecialAttack> getAttack() { return attacks; }
+    public int getAttack() { return attack; }
     public int getDefense() { return defense; }
-    public ArrayList<SpecialAttack> getSpecialAttack() { return attackSpes; }
+    public int getSpecialAttack() { return specialAttack; }
     public int getSpecialDefense() { return specialDefense; }
     public int getSpeed() { return speed; }
-    public Type getType1() { return type1; }
-    public Type getType2() { return type2; }
+    public String getType1() { return type1; }
+    public String getType2() { return type2; }
     public String getImageUrl() { return imageUrl; }
-    public int getAttackSpeStat() {return attackSpeStat;}
-    public int getAttackStat() {return attackStat;}
+
     public void setHp(int hp) {
         this.hp = Math.max(0, hp);
     }
