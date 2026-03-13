53b052ef4464b4337c09215c331bb5f21bd9533f
Cillian <baugercillianpro@gmail.com>
2026-03-13 13:07:43 +0100

    Creation of PokemonRepositories

--- src/main/java/org/example/pokemon/model/PokemonRepositories.java
+++ src/main/java/org/example/pokemon/model/PokemonRepositories.java
@@ -3,20 +3,42 @@ package org.example.pokemon.model;
 import org.example.pokemon.Manager.DataBaseManager;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PokemonRepositories {
     public List<Pokemon> getAllPokemon() {
         List<Pokemon> list = new ArrayList<>();
-        String sql = "SELECT * FROM pokemon";
+        String query = "SELECT * FROM pokemon";
 
         try{
-            Connection connection =
+            Connection conn = DataBaseManager.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                Pokemon p = new Pokemon(
+                        rs.getInt("id"),
+                        rs.getString("pokemon_name"),
+                        rs.getInt("hp"),
+                        rs.getInt("attack"),
+                        rs.getInt("defense"),
+                        rs.getInt("sp_attack"),
+                        rs.getInt("sp_defense"),
+                        rs.getInt("speed"),
+                        rs.getString("type1"),
+                        rs.getString("type2"),
+                        rs.getString("sprite_path")
+                );
+                list.add(p);
+            }
         }
         catch (Exception e){
             e.printStackTrace();
         }
+        return list;
     }
 
 
